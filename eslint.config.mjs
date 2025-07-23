55aa8f16288d114415ef6b06c9eeb93ae29c041a
Wilfred Njuguna <mwangiwilly395@gmail.com>
2025-07-23 13:19:22 +0300

    chore: setup Zod Validation and Tanstack Query using @heyapi-ts plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,7 @@ const eslintConfig = [
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/ban-ts-comment": "off",
+      "no-console": "error",
     },
   }),
 ]
