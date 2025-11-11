b635b6566abfcc78c6195dd7961af76ee26a988f
Wilfred Njuguna <mwangiwilly395@gmail.com>
2025-11-11 17:44:35 +0300

    chore: replace spinner loaders with skeletons

--- components/page-loader.tsx
+++ components/page-loader.tsx
@@ -1,9 +1,14 @@
-import Spinner from './ui/spinner';
+import { Skeleton } from '@/components/ui/skeleton';
 
 const PageLoader = () => {
   return (
-    <div className='col-span-full flex items-center justify-center py-6'>
-      <Spinner />
+    <div className='col-span-full w-full space-y-4 py-6'>
+      {Array.from({ length: 3 }).map((_, index) => (
+        <div key={index} className='space-y-2'>
+          <Skeleton className='h-4 w-1/3' />
+          <Skeleton className='h-6 w-full' />
+        </div>
+      ))}
     </div>
   );
 };
