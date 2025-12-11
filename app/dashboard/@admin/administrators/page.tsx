c90c187bf17ee6f86a1863197819df5793873a75
Wilfred Njuguna <mwangiwilly395@gmail,com>
2025-12-11 18:04:12 +0300

    Revert "ft: add admin creation drawer"
    This reverts commit aa8d6623df88a3c9f9d83d7a04d0c837701bd242.

--- app/dashboard/@admin/administrators/page.tsx
+++ app/dashboard/@admin/administrators/page.tsx
@@ -1,17 +1,13 @@
-import { AddAdminDrawer } from './_components/add-admin-drawer';
 import { AdminUserWorkspace } from '../users/_components/user-workspace';
 
 export default function AdminAdministratorsPage() {
   return (
-    <div className='space-y-6'>
-      <AddAdminDrawer />
-      <AdminUserWorkspace
-        title='Admin roster'
-        fixedDomain='admin'
-        useAdminEndpoint
-        emptyStateTitle='No administrators match your filters'
-        emptyStateDescription='Adjust search criteria or filters to find other administrators.'
-      />
-    </div>
+    <AdminUserWorkspace
+      title='Admin roster'
+      fixedDomain='admin'
+      useAdminEndpoint
+      emptyStateTitle='No administrators match your filters'
+      emptyStateDescription='Adjust search criteria or filters to find other administrators.'
+    />
   );
 }
