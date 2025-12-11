aa8d6623df88a3c9f9d83d7a04d0c837701bd242
Your Name <mwangiwilly395@gmail.com>
2025-12-11 17:31:59 +0300

    ft: add admin creation drawer

--- app/dashboard/@admin/administrators/page.tsx
+++ app/dashboard/@admin/administrators/page.tsx
@@ -1,13 +1,17 @@
+import { AddAdminDrawer } from './_components/add-admin-drawer';
 import { AdminUserWorkspace } from '../users/_components/user-workspace';
 
 export default function AdminAdministratorsPage() {
   return (
-    <AdminUserWorkspace
-      title='Admin roster'
-      fixedDomain='admin'
-      useAdminEndpoint
-      emptyStateTitle='No administrators match your filters'
-      emptyStateDescription='Adjust search criteria or filters to find other administrators.'
-    />
+    <div className='space-y-6'>
+      <AddAdminDrawer />
+      <AdminUserWorkspace
+        title='Admin roster'
+        fixedDomain='admin'
+        useAdminEndpoint
+        emptyStateTitle='No administrators match your filters'
+        emptyStateDescription='Adjust search criteria or filters to find other administrators.'
+      />
+    </div>
   );
 }
